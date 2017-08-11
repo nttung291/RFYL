4678b64be513a610ab69421e25ed1552bff6ecf6
DESKTOP-4O97OI9\Nttung PC <nttung291@gmail.com>
2017-08-12 00:16:55 +0700

    no message

--- src/game/scenes/Settings.java
+++
@@ -1,9 +0,0 @@
-package game.scenes;
-
-/**
- * Created by Nttung PC on 8/1/2017.
- */
-public class Settings {
-    public static float windowWidth = 1500;
-    public static float windowHeight = 600;
-}
--- src/inputs/InputListener.java
+++
@@ -1,9 +0,0 @@
-package inputs;
-
-/**
- * Created by Nttung PC on 8/3/2017.
- */
-public interface InputListener {
-    boolean onKeyPressed(int keyCode);
-    boolean onKeyReleased(int keyCode);
-}
